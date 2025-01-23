44648db90a6bb7a60eecd816e899aa4d44467300
Daniel Celestial <celestial.danielstephen@gmail.com>
2025-01-23 12:59:03 +0800

    Added CRUD buttons

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,13 +1,27 @@
 const Home = () => {
   return (
     <div className='bg-gray-100 w-screen h-screen'>
-        <div className="flex justify-center items-center w-screen h-screen">
-            <div>
+        <div className="flex flex-col justify-center items-center w-screen h-screen">
+            <div className="font-bold text-2xl">
                 <h2>ToDo List</h2>
             </div>
             <div>
-                <input className="bg-white" type="text" placeholder="Enter to do"></input>
-                <button>ADD</button>
+                <input className="bg-white p-2 w-72 outline-none border border-blue-300 rounded-md" type="text" placeholder="Enter to do"></input>
+                <button className="bg-blue-600 text-white p-2 m-4 rounded-sm">ADD</button>
+            </div> 
+
+            <div className="flex justify-between bg-white p-2 w-80">
+                <div>
+                    <p className="text-xl font-semi-bold">Do something</p>
+                    <p className="text-sm">Date today</p>
+                    <p className="text-sm">Status: Active</p>
+                </div>
+
+                <div className="flex flex-col">
+                    <button>Edit</button>
+                    <button>Delete</button>
+                    <button>Completed</button>
+                </div>
             </div>
         </div>
     </div>
