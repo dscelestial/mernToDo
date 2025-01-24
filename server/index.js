f3d420f215119164c57b6c32b087f9508dff1088
Daniel Celestial <celestial.danielstephen@gmail.com>
2025-01-24 13:39:59 +0800

    Backend setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mysql = require('mysql2');
 
 app.use(express.json());
 app.use(cors());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: '',
+    database: 'merntodo'
+})
+
+db.connect((err) => {
+    if(!err){
+        console.log("Connected to database successfully");
+    }
+    else{
+        console.log(err);
+    }
+})
+
+
+app.post('/addTask', (req,res) => {
+    console.log(req.body);
+})
 
 app.listen(5000, () => {
     console.log('Server started!')
